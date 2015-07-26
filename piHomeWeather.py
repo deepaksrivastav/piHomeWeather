4a2d89da1f9a57a6eee4d83edcefbeab39fea541
Deepak Srivastav <deepaksrivastav@gmail.com>
2015-07-26 17:41:05 +0200

    added a simple program which reports temperature and humidity

---
+++ piHomeWeather.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import sys
+import Adafruit_DHT
+
+humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+if humidity is not None and temperature is not None:
+        print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
+else:
+        print 'Failed to get reading. Try again!' 
