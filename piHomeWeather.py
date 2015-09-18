ebe46674db55940daba703c62e66a8779b1387cc
Deepak Srivastav <deepaksrivastav@gmail.com>
2015-09-18 22:01:16 +0200

    fixed code to read from gpio pin 23 and not using gpio #4

--- piHomeWeather.py
+++ piHomeWeather.py
@@ -3,7 +3,7 @@
 import sys
 import Adafruit_DHT
 
-humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 23)
 if humidity is not None and temperature is not None:
         print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
 else:
